07a275914a151cdecfe0525ba5c4b86766563765
MinhNhat <jimtruong13092003@gmail.com>
2023-10-31 10:18:25 +0700

    #3 - Cập nhật lại các phương thức lớp IDAO, ConnectDB

--- src/main/java/dev/skyherobrine/app/daos/IDAO.java
+++ src/main/java/dev/skyherobrine/app/daos/IDAO.java
@@ -1,115 +1,109 @@
 package dev.skyherobrine.app.daos;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Interface này được dùng để hiện thực và xử lý các phương thức liên quan đến vấn đề truy vấn trong
+ * CSDL để lấy dữ liệu.<br>
+ * Interface này cung cấp đầy đủ các loại tương tác dữ liệu: Insert - chèn dữ liệu, Update - cập nhật
+ * , Delete - xoá (nhưng bản chất là cập nhật tình trạng), Select - lấy và xem dữ liệu.
+ * @author Trương Dương Minh Nhật
+ * @param <T>
+ * @see T
+ * @version 2.0
+ */
 public interface IDAO<T>{
     /**
-     * Chèn thêm dữ liệu vào CSDL
+     * Chèn thêm dữ liệu vào trong CSDL.
+     * @param t
+     * @return <b>true</b> nếu chèn dữ liệu thành công, <b>false</b> nếu không thể nào chèn được dữ
+     * liệu.
+     * @throws Exception phương thức này sẽ ném ngoại lệ nếu rơi một trong những trường hợp: <u>sai
+     * cú pháp</u>, <u>không kết nối SQL được</u>, <u>trùng dữ liệu khoá chính</u>
+     * @since 1.0
+     */
+    boolean them(T t) throws Exception;
+
+    /**
+     * Cập nhật dữ liệu của một hoặc nhiều dữ liệu khác nhau tuỳ thuộc vào đối tượng đó có chứa mã hay
+     * không? Nếu đối tượng truyền vào không có chứa mã, tức là mã null (nếu mã là con số mã truyền số
+     * âm) thì khi cập nhật sẽ cập nhật cho nhiều dữ liệu khác nhau, nếu có mã thì chỉ cập nhật theo mã.
      * @param target
-     * @return <b>true</b>: nếu chèn thành công, <b>false</b>: nếu chèn thất bại, thông thường thất bại
-     * là <u>vi phạm ràng buộc</u>, <u>thiếu dữ liệu</u> hoặc <u>sai cú pháp</u>.
+     * @return {@link Integer} trả về số nguyên, chứa số lượng dòng bị ảnh hưởng.
+     * @throws Exception phương thức này sẽ ném ngoại lệ nếu rơi một trong những trường hợp: <u>sai
+     * cú pháp</u>, <u>không kết nối SQL được</u>, <u>không tìm thấy dữ liệu để cập nhật</u>
+     * @since 1.0
      */
-    boolean them(T target);
+    int capNhat(T target) throws Exception;
 
     /**
-     * Cập nhật dữ liệu của đối tượng theo mã (chỉ cập nhật 1 dữ liệu)
-     * @param target chứa tất cả dữ liệu của đối tượng để cập nhật, các dữ liệu liên quan đến khoá chính sẽ
-     * tìm kiếm dữ liệu trong CSDL và các dữ liệu còn lại được hiểu là để cập nhật, thay đổi giá trị.
-     * @return <b>true</b>: nếu tìm được đối tượng, <b>false</b>: nếu không tìm được đối tượng, thông thường thất bại
-     * do <u>không tìm được đối tượng</u>, <u>có các giá trị trong thuộc tính null hoặc rỗng</u>,
-     * <u>vi phạm ràng buộc dữ liệu</u>
+     * Xoá dữ liệu của một đối tượng thông qua mã ID của đối tượng đó.
+     * @param id
+     * @return <b>true</b> - nếu tìm được đối tượng và đã cập nhật hay xoá (tuỳ tình huống) dữ liệu,
+     * hoặc <b>false</b> - nếu không tìm được đối tượng
+     * @throws Exception phương thức này sẽ ném ngoại lệ nếu rơi một trong những trường hợp: <u>sai
+     * cú pháp</u>, <u>không kết nối SQL được</u>, <u>không tìm thấy dữ liệu để xoá</u>
+     * @since 1.0
      */
-    boolean capNhatTheoMa(T target);
+    boolean xoa(String id) throws Exception;
 
     /**
-     * Cập nhật các dữ liệu theo các thuộc tính, việc cập nhật như thế này sẽ cập nhật hết toàn bộ dữ liệu
-     * @param targets sẽ là tham số chứa danh sách các index (chỉ mục), các chỉ muc này gồm có 2 trường dữ
-     * liệu (key-value):
-     * <ul>
-     *                <li><b>Key</b>: đại diện cho tên thuộc tính sẽ được cập nhật, thay đổi giá trị</li>
-     *                <li><b>Value</b>: giá trị mà ứng với key đó sẽ được cập nhật</li>
-     * </ul>
-     * @return <b>{@link Map}</b> trả về một cặp giá trị key-value (Boolean - Integer), với <b>key</b> là kiểu {@link Boolean}
-     * ứng với việc câu lệnh truy vấn có chạy thành công hay không? Và <b>value</b> là kiểu {@link Integer}, dùng để trả
-     * lại số dòng được thực thi ứng với tuỳ câu lệnh tương tác CSDL:
-     * <ul>
-     *     <li>Create: sẽ trả về 0 hoặc 1 (thường dùng để tạo bảng)</li>
-     *     <li>Read: sẽ trả về 0 hoặc nhiều hơn</li>
-     *     <li>Update: sẽ trả về 0 hoặc nhiều hơn</li>
-     *     <li>Delete: sẽ trả về 0 hoặc nhiều hơn</li>
-     * </ul>
+     * Xoá dữ liệu của nhiều đối tượng không qua nhiều ID khác nhau. Phương thức sẽ thực hiện truy vấn
+     * xoá các dòng dữ liệu của bảng trong CSDL theo mảng chứa các ID của tham số.
+     * @param ids chứa danh sách tất cả các mã ID của dữ liệu.
+     * @return {@link Integer} - trả về số nguyên, số lượng dòng bị xoá.
+     * @throws Exception phương thức này sẽ ném ngoại lệ nếu rơi một trong những trường hợp: <u>sai
+     * cú pháp</u>, <u>không kết nối SQL được</u>
+     * @since 1.0
      */
-    Map<Boolean, Integer> capNhatTheoThuocTinh(Map<String, Object> targets);
+    int xoa(String...ids) throws Exception;
 
     /**
-     * Cập nhật các dữ liệu theo các thuộc tính, việc cập nhật như thế này sẽ cập nhật hết toàn bộ dữ liệu
-     * @param targets sẽ là tham số chứa danh sách các index (chỉ mục), các chỉ muc này gồm có 2 trường dữ
-     * liệu (key-value):
-     * <ul>
-     *                <li><b>Key</b>: đại diện cho tên thuộc tính sẽ được cập nhật, thay đổi giá trị</li>
-     *                <li><b>Value</b>: giá trị mà ứng với key đó sẽ được cập nhật</li>
-     * </ul>
-     * @param conditions sẽ là tham số chứa danh sách các index (chỉ mục), các chỉ mục này gồm có 2 trường
-     * dữ liệu (key-value):
-     * <ul>
-     *                   <li><b>Key</b>: đại diện cho tên thuộc tính sẽ kiểm tra</li>
-     *                   <li><b>Value</b>: giá trị mà ứng với key đó sẽ kiểm tra dữ liệu có khớp giá trị không?</li>
-     * </ul>
-     * @return <b>{@link Map}</b> trả về một cặp giá trị key-value (Boolean - Integer), với <b>key</b> là kiểu {@link Boolean}
-     * ứng với việc câu lệnh truy vấn có chạy thành công hay không? Và <b>value</b> là kiểu {@link Integer}, dùng để trả
-     * lại số dòng được thực thi ứng với tuỳ câu lệnh tương tác CSDL:
-     * <ul>
-     *     <li>Create: sẽ trả về 0 hoặc 1 (thường dùng để tạo bảng)</li>
-     *     <li>Read: sẽ trả về 0 hoặc nhiều hơn</li>
-     *     <li>Update: sẽ trả về 0 hoặc nhiều hơn</li>
-     *     <li>Delete: sẽ trả về 0 hoặc nhiều hơn</li>
-     * </ul>
+     * Lấy danh sách các dữ liệu của bảng {@link T} trong CSDL. Phương thức sẽ thực hiện truy vấn lấy
+     * danh sách các dữ liệu tồn tại mà không đi kèm bất kì điều kiện truy vấn nào.
+     * @return {@link List} - trả về danh sách các phần từ, ứng với mỗi phần tử là một đối tượng {@link T}
+     * @throws Exception phương thức này sẽ ném ngoại lệ nếu rơi một trong những trường hợp: <u>sai
+     * cú pháp</u>, <u>không kết nối SQL được</u>
+     * @since 1.0
      */
-    Map<Boolean, Integer> capNhatTheoThuocTinh(Map<String, Object> targets, Map<String, Object> conditions);
+    List<T> timKiem() throws Exception;
 
     /**
-     * Tìm kiếm đối tượng thông qua mã
-     * @param ma sẽ được truy vấn để lấy dữ liệu.
-     * @return <b>{@link Optional}</b>: trả về đối tượng thông qua Optional bao gồm cả có cả dữ liệu và không có dữ liệu.</b>
+     * Lấy danh sách các dữ liệu của bảng {@link T} trong CSDL theo điều kiện trong tham số. Phương thức
+     * này sẽ thực hiện truy vấn lấy danh sách các dữ liệu thoả mãn điều kiện trong câu truy vấn.
+     * @param conditions là một {@link Map} thuộc {@link java.util.Collection} chứa danh sách các phần tử
+     * tuân theo quy tắc <b>Key - Value</b>.
+     * @return {@link List} - danh sách các phần tử khớp với câu lệnh truy vấn CSDL. Mỗi một phần tử trong
+     * danh sách ứng với đối tượng {@link T}
+     * @throws Exception phương thức này sẽ ném ngoại lệ nếu rơi một trong những trường hợp: <u>sai
+     * cú pháp</u>, <u>không kết nối SQL được</u>
+     * @since 1.0
      */
-    Optional<T> timKiemTheoMa(String ma);
+    List<T> timKiem(Map<String, Object> conditions) throws Exception;
 
     /**
-     * Tìm kiếm các dối tượng thông các mã tuỳ chọn.
-     * @param dsMa sẽ chứa toàn bộ mã theo đối tượng T để lấy danh sách dữ liệu
-     * @return <b>{@link List}</b> trả về danh sách các đối tượng chứa dữ liệu {@link T}
+     * Tìm kiếm một đối tượng theo mã id trong CSDL.
+     * @param id
+     * @return {@link Optional} - một đối tượng container chứa một giá trị mang {@link T}, đối tượng container
+     * sẽ khử việc kết quả trả về bị null, để tránh tình trạng chương trình bị exception về {@link NullPointerException}
+     * @throws Exception phương thức này sẽ ném ngoại lệ nếu rơi một trong những trường hợp: <u>sai
+     * cú pháp</u>, <u>không kết nối SQL được</u>
+     * @since 1.0
      */
-    List<T> timKiemTheoMa(String...dsMa);
+    Optional<T> timKiem(String id) throws Exception;
 
     /**
-     * Lấy danh sách tất cả các đối tượng
-     * @return <b>{@link List}</b> chứa danh sách tất cả các đối tượng, nếu như trong danh sách không có đối
-     * tượng nào thì nó sẽ trả về null
+     * Lấy danh sách các dữ liệu của bảng {@link T} trong CSDL theo các mã ID đã được liệt kê trong tham
+     * số. Phương thức này sẽ truy vấn, tìm kiếm các dữ liệu khớp với một trong danh sách các mã trong
+     * truy vấn.
+     * @param ids
+     * @return {@link List} - danh sách các phần tử, mỗi phần tử ứng với đối tượng {@link T}
+     * @throws Exception phương thức này sẽ ném ngoại lệ nếu rơi một trong những trường hợp: <u>sai
+     * cú pháp</u>, <u>không kết nối SQL được</u>
+     * @since 1.0
      */
-    List<T> timKiemDS();
-
-    /**
-     * Lấy danh sách của tất cả các đối tượng theo điều kiện
-     * @param targets sẽ là tham số chứa danh sách các index (chỉ mục), các chỉ muc này gồm có 2 trường dữ
-     * liệu (key-value):
-     * <ul>
-     *                <li><b>Key</b>: đại diện cho tên thuộc tính sẽ kiểm tra</li>
-     *                <li><b>Value</b>: giá trị mà ứng với tên thuộc tính đó sẽ kiểm tra</li>
-     * </ul>
-     * @return <b>{@link Map}</b> trả về một đối tượng theo giá trị Key-Value, vói key là {@link Boolean} sẽ
-     * quyết định kết quả của câu truy vấn có được thực thi hay không, còn value là {@link List} sẽ chứa toàn
-     * bộ giá trị kết quả thu thập được khi truy vấn CSDL theo đối tượng {@link T}
-     */
-    Map<Boolean, List<T>> timKiemDS(Map<String, Object> targets);
-
-    /**
-     * Lấy danh sách tất cả các đối tượng (nhưng lấy một số tên cột ra nên nó không đầy đủ giá trị)
-     * @param columns chứa danh sách tất cả các tên cột của CSDL cần được truy vấn tới.
-     * @return <b>{@link Map}</b> trả về một đối tượng theo giá trị Key-Value, vói key là {@link Boolean} sẽ
-     * quyết định kết quả của câu truy vấn có được thực thi hay không, còn value là {@link List} sẽ chứa toàn
-     * bộ giá trị kết quả thu thập được khi truy vấn CSDL theo đối tượng {@link T}
-     */
-    Map<Boolean, List<T>> timKiemDS(String...columns);
+    List<T> timkiem(String...ids) throws Exception;
 }
