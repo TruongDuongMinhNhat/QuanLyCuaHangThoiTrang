9e6ea10a15386d8f77e41bd553abb00e5eb5ffaf
virtue1803 <nguyenductai505@gmail.com>
2023-12-08 21:52:23 +0700

    #5- thêm nút huỷ cho table

---
+++ src/main/java/dev/skyherobrine/app/views/dashboard/component/nutHuy/TableActionEvent.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.skyherobrine.app.views.dashboard.component.nutHuy;
+
+/**
+ *
+ * @author Virtue Nguyen
+ */
+public interface TableActionEvent {
+    public void onHuy(int row);
+}
